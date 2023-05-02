1fac03d511f10e0416fd7b377ed05e37ef408d7d
JR <www.aereregion.webs.com@live.co.uk>
2023-05-02 16:57:50 +0100

    feat(demo): adds the CoilImageLoader.kt object for the demo app

---
+++ demoapp/src/main/java/com/cube/fusion/android/demoapp/images/CoilImageLoader.kt
@@ -0,0 +1,23 @@
+package com.cube.fusion.android.demoapp.images
+
+import android.widget.ImageView
+import coil.load
+import com.cube.fusion.android.core.images.FusionAndroidImageLoader
+import com.cube.fusion.android.demoapp.R
+import com.cube.fusion.core.model.views.Image
+
+/**
+ * [FusionAndroidImageLoader] implementation based off of the Coil image loading library
+ * Other image loading libs for Android are available
+ *
+ * Created by JR Mitchell on 02/May/2023.
+ * Copyright ® 3SidedCube. All rights reserved.
+ */
+object CoilImageLoader: FusionAndroidImageLoader {
+	override fun loadImage(image: Image?, view: ImageView) {
+		view.load(image?.src?.url) {
+			placeholder(R.mipmap.ic_placeholder)
+			error(R.mipmap.ic_placeholder)
+		}
+	}
+}
